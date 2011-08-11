6a7f1091aacb66f3f5393321d006c77ac498a6f8
Matt Schneeberger <helluvamatt@gmail.com>
2011-08-10 23:56:18 -0500

    Initial commit, with plugin skeleton.

---
+++ src/com/schneenet/tarati/TaratiPermissionsPlugin.java
@@ -0,0 +1,21 @@
+package com.schneenet.tarati;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import ru.tehkode.permissions.PermissionManager;
+
+public class TaratiPermissionsPlugin extends JavaPlugin {
+	
+	private PermissionManager permissionsManager;
+	
+	@Override
+	public void onDisable() {
+		
+	}
+
+	@Override
+	public void onEnable() {
+		
+	}
+
+}
