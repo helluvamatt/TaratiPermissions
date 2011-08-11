5b62174e9281956b5b28a9664e74861297c1f6ea
Matt Schneeberger <helluvamatt@gmail.com>
2011-08-11 00:51:31 -0500

    Custom logging class and we now hook into PermissionsEx.

--- src/com/schneenet/tarati/TaratiPermissionsPlugin.java
+++ src/com/schneenet/tarati/TaratiPermissionsPlugin.java
@@ -1,12 +1,16 @@
 package com.schneenet.tarati;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import ru.tehkode.permissions.PermissionManager;
+import ru.tehkode.permissions.bukkit.PermissionsEx;
 
 public class TaratiPermissionsPlugin extends JavaPlugin {
 	
-	private PermissionManager permissionsManager;
+	private PermissionManager permissionManager;
+	private String pluginName;
+	private String pluginVersion;
 	
 	@Override
 	public void onDisable() {
@@ -15,6 +19,20 @@ public class TaratiPermissionsPlugin extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
+		
+		// Basic Plugin Setup
+		pluginName = this.getDescription().getName();
+		pluginVersion = this.getDescription().getVersion();
+		TaratiPermissionsLogger.info("Enabling " + pluginName + " v" + pluginVersion);
+		
+		// Hook into PermissionsEx 
+		Plugin permissionsEx = this.getServer().getPluginManager().getPlugin("PermissionsEx");
+		if (permissionsEx != null) {
+			permissionManager = PermissionsEx.getPermissionManager();
+			TaratiPermissionsLogger.info("Hooked into " + permissionsEx.getDescription().getName() + " (v" + permissionsEx.getDescription().getVersion() + ").");
+		} else {
+			TaratiPermissionsLogger.warn("PermissionsEx plugin was not found or enabled.");
+		}
 		
 	}
 
